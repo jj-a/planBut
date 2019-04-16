ffd637d4f30b64755fc25c958d356e0a42cb0169
Jun <vagrantsj@gmail.com>
2019-04-16 18:33:23 +0900

    (plan) 컨트롤러, jsp, mapper

---
+++ src/main/java/kr/co/planbut/plan/PlanController.java
@@ -0,0 +1,24 @@
+package kr.co.planbut.plan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PlanController {
+/*
+	@Autowired
+	PlanDAO dao;
+ */
+	public PlanController() {
+		System.out.println("------PlanController() 객체 생성됨");
+	}// planController() end
+	
+	@RequestMapping(value="/plan/plan.do", method=RequestMethod.GET)
+	public ModelAndView createForm() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("plan/plan");
+		return mav;
+	}//creatForm end
+}// class end
