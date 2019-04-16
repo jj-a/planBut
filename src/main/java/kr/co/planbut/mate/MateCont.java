406b07f423f151813475c4b9b500ded227d98c5f
nstella67 <leenuri67@gmail.com>
2019-04-16 17:48:20 +0900

    MateDTO

--- src/main/java/kr/co/planbut/mate/MateCont.java
+++ src/main/java/kr/co/planbut/mate/MateCont.java
@@ -1,5 +1,5 @@
 package kr.co.planbut.mate;
 
-public class MateBbsCont {
+public class MateCont {
 
 }
--- src/main/java/kr/co/planbut/mate/MateDAO.java
+++ src/main/java/kr/co/planbut/mate/MateDAO.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MateBbsDAO {
+public class MateDAO {
 	
 	// POJO 방식의 DB 연결
 	// DBOpen dbopen;
@@ -20,7 +20,7 @@ public class MateBbsDAO {
 	@Autowired
 	SqlSession sqlSession;
 	
-	public MateBbsDAO() {
+	public MateDAO() {
 		System.out.println("����������� MateDAO() 객체 생성 �����������");
 	}
 	
